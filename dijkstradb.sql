19165845289e605737c6f13dc54ea6c641010129
Francesco Fortini <31167775+Franx98F@users.noreply.github.com>
2024-11-21 19:33:06 +0100

    correct-dijkstradb

--- dijkstradb.sql
+++ dijkstradb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Nov 21, 2024 alle 19:13
+-- Creato il: Nov 21, 2024 alle 19:32
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `dijkstradb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `edges`
+--
+
+CREATE TABLE `edges` (
+  `id` int(11) NOT NULL,
+  `fromNode` int(11) NOT NULL,
+  `toNode` int(11) NOT NULL,
+  `weight` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `nodes`
+--
+
+CREATE TABLE `nodes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,6 +65,21 @@ CREATE TABLE `results` (
 --
 
 --
+-- Indici per le tabelle `edges`
+--
+ALTER TABLE `edges`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fromNode` (`fromNode`),
+  ADD KEY `toNode` (`toNode`);
+
+--
+-- Indici per le tabelle `nodes`
+--
+ALTER TABLE `nodes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
 -- Indici per le tabelle `results`
 --
 ALTER TABLE `results`
@@ -51,10 +90,33 @@ ALTER TABLE `results`
 --
 
 --
+-- AUTO_INCREMENT per la tabella `edges`
+--
+ALTER TABLE `edges`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT per la tabella `nodes`
+--
+ALTER TABLE `nodes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT per la tabella `results`
 --
 ALTER TABLE `results`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `edges`
+--
+ALTER TABLE `edges`
+  ADD CONSTRAINT `edges_ibfk_1` FOREIGN KEY (`fromNode`) REFERENCES `nodes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `edges_ibfk_2` FOREIGN KEY (`toNode`) REFERENCES `nodes` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
